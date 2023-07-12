800406ca32a0bcc57034ed77ed563c7a6ac71a4d
David Rolón <rolondavid@outlook.com>
2023-07-11 23:09:20 -0300

    first commit

---
+++ src/main/java/com/rain_lovers/huertas_agroecologicas/HuertasAgroecologicasApplication.java
@@ -0,0 +1,13 @@
+package com.rain_lovers.huertas_agroecologicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuertasAgroecologicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuertasAgroecologicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rain_lovers/huertas_agroecologicas/HuertasAgroecologicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rain_lovers.huertas_agroecologicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuertasAgroecologicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
