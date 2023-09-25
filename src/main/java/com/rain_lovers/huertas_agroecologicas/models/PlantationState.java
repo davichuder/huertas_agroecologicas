ac4b55807f4a4d436407eb8fcb7a8dca86bd7efa
Alexandro Moroz <AlexandroMoroz5@gmail.com>
2023-09-25 20:50:24 -0300

    build: :construction: Creación del modelo "PlantationState"
    Se creó el modelo "PlantationState", se modificó el script "V2" para crear su tabla y se creó el Enum que usa este mismo modelo.

--- src/main/java/com/rain_lovers/huertas_agroecologicas/models/PlantationState.java
+++ src/main/java/com/rain_lovers/huertas_agroecologicas/models/PlantationState.java
@@ -1,5 +1,24 @@
 package com.rain_lovers.huertas_agroecologicas.models;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
 public class PlantationState {
 
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    @NotNull
+    private PlantationStateEnum state;
 }
